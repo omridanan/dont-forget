3cfc1f118e48316565c6ab0d1c7813fe2741f019
bityob <bityob@gmail.com>
2017-06-07 18:36:07 +0000

    Finished for now set task leader by group

--- server/jobs/set_task_leader_by_group.py
+++ server/jobs/set_task_leader_by_group.py
@@ -27,9 +27,41 @@ def run():
     for tasks_group in tasks_groups:
         
         tasks = tasks_group['tasks']
+        task_group_id = tasks_groups['_id']
+        entities_count = db.entities_group.find_one({'_id': ObjectId(task_group_id)})['entities_count']
+        all_entities_count = float(sum(entities_count.values())) # float needed for divide (3 / 100 = 0.03, not 0)
+        
+        sum_tasks = {}
         
         for task_id in tasks:
             task = db.tasks.find_one({'_id': ObjectId(task_id)})
+            sum_task = 0
+            
+            entities_for_task = db.entities.find_one({'_id': ObjectId(task_group_id)})
+            
+            for entity_id in entities_for_task:
+                
+                entity = db.tasks.find_one({'_id': ObjectId(entity_id)})
+                
+                entity_count = entities_count['entity_id']
+                
+                entity_percentage = float(entity_count) / all_entities_count
+                
+                entity_val = entity['salience'] * entity_percentage
+                
+                sum_task += entity_val
+                
+                sum_tasks[task_id] = sum_task
+                
+        
+        # Get max sum_task by task
+        max_task_id = max(sum_tasks)
+        
+        # Set as task leader
+        db.tasks_group.update_one(
+            {'_id': ObjectId(task_group_id)}, 
+            { '$set' : { "taskLeaderId" : task_id  }
+        )
             
             
         
