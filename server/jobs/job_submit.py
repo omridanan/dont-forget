65514ccae1565e1003ef2421a1ace174bf37b97d
bityob <bityob@gmail.com>
2017-07-30 19:38:14 +0000

    push jobs - wip

--- server/jobs/job_submit.py
+++ server/jobs/job_submit.py
@@ -1,15 +1,19 @@
 from redis import Redis
 from rq import Queue
 from task_processor import processTask
+import test_rq
 
 q = Queue(connection=Redis())
 
 def run_task_flow_manager(person_id, task_id):
     q.enqueue(processTask, person_id, task_id)
     
+def rq_test():
+    q.enqueue(test_rq.run)
     
 if __name__ == '__main__':
-    run_task_flow_manager(person_id, task_id)
+    # run_task_flow_manager(person_id, task_id)
+    rq_test()
     
    
 
